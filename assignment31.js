921bd09f10187cee741328a9a5a23c7bba56327d
RafaLevina06 <brafalevina06@gmail.com>
2024-01-16 15:04:55 +0700

    changed assignment excercise 31

--- assignment31.js
+++ assignment31.js
@@ -9,22 +9,6 @@ function isArithmeticProgression(numbers) {
     return true
 }
 
-// function isArithmeticProgression(numbers) {
-//     let result = true
-
-//     for(let i = 0; i < numbers.length - 1; i++) {
-//         if (numbers[i+1] - numbers[i] === numbers[0]){
-//             result = true
-//         } else {
-//             result = false
-//         }
-//         if ( !result) {
-//             break;
-//         }
-//     }
-//     return result
-// }
-
 console.log(isArithmeticProgression([1, 2, 3, 4, 5, 6]));
 console.log(isArithmeticProgression([2, 4, 6, 12, 24]));
 console.log(isArithmeticProgression([2, 4, 6, 8]));
